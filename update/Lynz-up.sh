29f7be32fed83f10c5ff6755d8249fa9d097dc94
Afabiizni <136027286+Afabiizni@users.noreply.github.com>
2023-07-12 17:03:14 +0800

    Add files via upload

---
+++ update/Lynz-up.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+BG="\E[44;1;39m"
+
+NC="\E[0m"
+
+echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m            INFORMASI UPDATE SCRIPT         \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+
+echo -e "      •> Add UDP Server <•" | lolcat
+
+echo -e "    •> Install UDP Server <•" | lolcat
+
+echo -e "      •> Fix Bug Script <•" | lolcat
+
+echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m                THANKS FOR UPDATE           \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+
+sleep 3
+echo -e " [INFO] Downloading Update File"
+sleep 2
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Afabiizni/xiao/main/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Afabiizni/xiao/main/menu/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
+
+
+echo -e " [INFO] Installing SSH UDP"
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2" -O install-udp && rm -rf /tmp/cookies.txt && chmod +x install-udp && ./install-udp
+
+
+echo -e " [INFO] UPDATE BERHASIL..."
+sleep 2
+menu
